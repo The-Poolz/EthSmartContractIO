7adc499331a8d51f5df36be42696e454c0b20315
ArdenHide <ardenhide@gmail.com>
2023-05-24 15:29:01 +0300

    add JsonRpcRequest

---
+++ src/RPC.Core/Models/JsonRpcRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace RPC.Core.Models;
+
+public class JsonRpcRequest
+{
+    public JsonRpcRequest(string to, string data)
+    {
+        JsonRpc = "2.0";
+        Method = "eth_call";
+        Params = new JArray()
+        {
+            new JObject()
+            {
+                { "to", to },
+                { "data", data }
+            },
+            "latest"
+        };
+        Id = 0;
+    }
+    public string JsonRpc { get; set; }
+    public string Method { get; set; }
+    public JArray Params { get; set; }
+    public int Id { get; set; }
+}
