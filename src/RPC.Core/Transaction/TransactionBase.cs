122992a153d9048b049065c62f9f2565a1b77ede
ArdenHide <ardenhide@gmail.com>
2023-05-25 12:36:15 +0300

    update code

---
+++ src/RPC.Core/Transaction/TransactionBase.cs
@@ -0,0 +1,13 @@
+﻿using Nethereum.Web3;
+
+namespace RPC.Core.Transaction;
+
+public abstract class TransactionBase
+{
+    protected readonly IWeb3 web3;
+
+    protected TransactionBase(IWeb3 web3)
+    {
+        this.web3 = web3;
+    }
+}
---
+++ src/RPC.Core/Transaction/TransactionSigner.cs
@@ -0,0 +1,14 @@
+﻿using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace RPC.Core.Transaction;
+
+public class TransactionSigner : TransactionBase
+{
+    public TransactionSigner(IWeb3 web3) : base(web3) { }
+
+    public virtual string SignTransaction(TransactionInput transaction) =>
+        web3.TransactionManager.Account.TransactionManager.SignTransactionAsync(transaction)
+            .GetAwaiter()
+            .GetResult();
+}
