93f1b8fd73d099bd159b58d2cb18d66429046e0e
ArdenHide <ardenhide@gmail.com>
2023-05-25 12:46:40 +0300

    update tests

---
+++ tests/RPC.Core.Tests/Transaction/TransactionSenderTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using RPC.Core.Tests.Mocks;
+
+namespace RPC.Core.Transaction.Tests;
+
+public class TransactionSenderTests
+{
+    [Fact]
+    internal void SignTransaction_ExpectedTransaction()
+    {
+        var manager = new TransactionSender(MockWeb3.GetMock);
+
+        var txHash = manager.SendTransaction("signedTransaction");
+
+        Assert.NotNull(txHash);
+        Assert.Equal("transactionHash", txHash);
+    }
+}
