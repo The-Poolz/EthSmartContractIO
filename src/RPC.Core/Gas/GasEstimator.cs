c52edbd8f92bbe6293da89578b12ea90f367e122
ArdenHide <ardenhide@gmail.com>
2023-05-25 13:09:23 +0300

    add code

---
+++ src/RPC.Core/Gas/GasEstimator.cs
@@ -0,0 +1,25 @@
+﻿using Nethereum.Web3;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace RPC.Core.Gas;
+
+public class GasEstimator : GasBase
+{
+    public const int GasBufferFactor = 10;
+
+    public GasEstimator(IWeb3 web3) : base(web3) { }
+
+    public TransactionInput EstimateGas(TransactionInput transaction)
+    {
+        var gasEstimate = web3.Eth.TransactionManager.EstimateGasAsync(transaction)
+            .GetAwaiter()
+            .GetResult();
+
+        var bufferOfGasLimit = new HexBigInteger(gasEstimate.Value / GasBufferFactor);
+
+        transaction.Gas = new HexBigInteger(gasEstimate.Value + bufferOfGasLimit.Value);
+
+        return transaction;
+    }
+}
---
+++ src/RPC.Core/Gas/GasPricer.cs
@@ -0,0 +1,14 @@
+﻿using Nethereum.Web3;
+using Nethereum.Hex.HexTypes;
+
+namespace RPC.Core.Gas;
+
+public class GasPricer : GasBase
+{
+    public GasPricer(IWeb3 web3) : base(web3) { }
+
+    public HexBigInteger GetCurrentWeiGasPrice() =>
+        web3.Eth.GasPrice.SendRequestAsync()
+            .GetAwaiter()
+            .GetResult();
+}
