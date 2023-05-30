bdb4fd81e4d165a897fc66cae6cfe3b07d51a095
ArdenHide <ardenhide@gmail.com>
2023-05-30 17:36:01 +0300

    update RequestValidator

--- src/RPC.Core/Validation/RequestValidator.cs
+++ src/RPC.Core/Validation/RequestValidator.cs
@@ -8,37 +8,60 @@ public class RequestValidator : AbstractValidator<Request>
 {
     public RequestValidator()
     {
-        RuleFor(x => x.RpcUrl)
-            .NotEmpty()
-            .When(x => x.ActionType == ActionType.Read);
+        RuleFor(x => x.To).NotNull().NotEmpty();
 
+        When(x => x.ActionType == ActionType.Read, RequireReadParameters);
+
+        When(x => x.ActionType == ActionType.Write, RequireWriteParameters);
+
+        When(x => x.ActionType == ActionType.Read, EnsureWriteParametersAreNull);
+
+        When(x => x.ActionType == ActionType.Write, EnsureReadParametersAreNull);
+    }
+
+    private void RequireReadParameters()
+    {
+        RuleFor(x => x.RpcUrl).NotEmpty();
+
+        RuleFor(x => x.Data).NotEmpty();
+    }
+
+    private void RequireWriteParameters()
+    {
+        RuleFor(x => x.Web3).NotNull();
+
+        RuleFor(x => x.ChainId).NotEqual(default(uint));
+
+        RuleFor(x => x.From).NotNull().NotEmpty();
+
+        RuleFor(x => x.Value).NotNull();
+
+        RuleFor(x => x.GasSettings).NotNull().SetValidator(new GasSettingsValidator());
+    }
+
+    private void EnsureWriteParametersAreNull()
+    {
         RuleFor(x => x.Web3)
-            .NotNull()
-            .When(x => x.ActionType == ActionType.Write);
-
-        RuleFor(x => x.To)
-            .NotNull().NotEmpty();
-
-        RuleFor(x => x.Data)
-            .NotEmpty()
-            .When(x => x.ActionType == ActionType.Read);
-
-        RuleFor(x => x.ChainId)
-            .NotEqual(default(uint))
-            .When(x => x.ActionType == ActionType.Write);
+            .Null()
+            .WithMessage("Web3 must be null for Read operations");
 
         RuleFor(x => x.From)
-            .NotNull()
-            .NotEmpty()
-            .When(x => x.ActionType == ActionType.Write);
+            .Empty()
+            .WithMessage("From must be null for Read operations");
 
         RuleFor(x => x.Value)
-            .NotNull()
-            .When(x => x.ActionType == ActionType.Write);
+            .Null()
+            .WithMessage("Value must be null for Read operations");
 
         RuleFor(x => x.GasSettings)
-            .NotNull()
-            .SetValidator(new GasSettingsValidator())
-            .When(x => x.ActionType == ActionType.Write);
+            .Null()
+            .WithMessage("GasSettings must be null for Read operations");
+    }
+
+    private void EnsureReadParametersAreNull()
+    {
+        RuleFor(x => x.RpcUrl)
+            .Empty()
+            .WithMessage("RpcUrl must be null for Write operations");
     }
 }
