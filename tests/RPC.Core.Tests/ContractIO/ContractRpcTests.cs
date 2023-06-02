53a4a744f13b2f446ef829cfe8fd075ab225e378
ArdenHide <ardenhide@gmail.com>
2023-06-02 11:35:32 +0300

    Add read tests in ContractRpc

---
+++ tests/RPC.Core.Tests/ContractIO/ContractRpcTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+using RPC.Core.Models;
+using Flurl.Http.Testing;
+using Newtonsoft.Json.Linq;
+
+namespace RPC.Core.ContractIO.Tests;
+
+public class ContractRpcTests
+{
+    private const string RpcUrl = "http://localhost:8545/";
+
+    [Fact]
+    internal void ExecuteAction_Read_ExpectedJsonString()
+    {
+        var request = new Request(RpcUrl, "0xA98b8386a806966c959C35c636b929FE7c5dD7dE", "0xbef7a2f0");
+        var response = new JObject()
+        {
+            { "jsonrpc", "2.0" },
+            { "result", "0x000000000000000000000000000000000000000000000000002386f26fc10000" },
+            { "id", 0 }
+        }.ToString();
+        using var httpTest = new HttpTest();
+        httpTest
+            .ForCallsTo(RpcUrl)
+            .RespondWithJson(response);
+
+        var result = new ContractRpc().ExecuteAction(request);
+
+        Assert.NotNull(result);
+        Assert.Equal(response, result);
+    }
+}
