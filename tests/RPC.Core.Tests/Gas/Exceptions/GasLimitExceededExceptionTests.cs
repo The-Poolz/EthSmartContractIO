c397267def985387778130bcc943d7e0da922c86
ArdenHide <ardenhide@gmail.com>
2023-06-05 17:41:38 +0300

    add tests

---
+++ tests/RPC.Core.Tests/Gas/Exceptions/GasLimitExceededExceptionTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System.Text.Json;
+
+namespace RPC.Core.Gas.Exceptions.Tests;
+
+public class GasLimitExceededExceptionTests
+{
+    [Fact]
+    public void GasLimitExceededException_SerializationTest()
+    {
+        var originalException = new GasLimitExceededException();
+        GasLimitExceededException deserializedException;
+
+        var jsonString = JsonSerializer.Serialize(originalException);
+        deserializedException = JsonSerializer.Deserialize<GasLimitExceededException>(jsonString)!;
+
+        Assert.Equal(originalException.Message, deserializedException.Message);
+    }
+}
