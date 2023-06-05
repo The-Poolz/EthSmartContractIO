02cf5cedfaa5995cf5b917ff5ddeb97a7e0fbb1a
ArdenHide <ardenhide@gmail.com>
2023-06-05 16:07:14 +0300

    add one more test

--- tests/RPC.Core.Tests/ContractIO/ContractRpcReaderTests.cs
+++ tests/RPC.Core.Tests/ContractIO/ContractRpcReaderTests.cs
@@ -14,7 +14,6 @@ public class ContractRpcReaderTests
         { "result", "0x000000000000000000000000000000000000000000000000002386f26fc10000" },
         { "id", 0 }
     };
-
     [Fact]
     internal void RunContractAction_ShouldReturnExpectedJsonString()
     {
@@ -28,5 +27,32 @@ public class ContractRpcReaderTests
 
         Assert.NotNull(result);
         Assert.Equal(response["result"]?.ToObject<string>(), result);
+    }
+
+    [Fact]
+    internal void RunContractAction_ShouldReturnExpectedEmptyString()
+    {
+        var errorResponse = new JObject()
+        {
+            { "jsonrpc", "2.0" },
+            {
+                new JObject()
+                {
+                    { "code", -32602 },
+                    { "message", "missing value for required argument 0" }
+                }
+            },
+            { "id", 0 }
+        };
+        var request = new RpcRequest(RpcUrl, "0xA98b8386a806966c959C35c636b929FE7c5dD7dE", "0xbef7a2f0");
+        using var httpTest = new HttpTest();
+        httpTest
+            .ForCallsTo(RpcUrl)
+            .RespondWithJson(errorResponse);
+
+        var result = new ContractRpcReader(request).RunContractAction();
+
+        Assert.NotNull(result);
+        Assert.Equal(string.Empty, result);
     }
 }
