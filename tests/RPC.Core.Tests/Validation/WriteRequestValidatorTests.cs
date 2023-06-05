cc19145d4a0dc0d7a104e7a963637a7cecac5c1d
ArdenHide <ardenhide@gmail.com>
2023-06-05 16:00:48 +0300

    fix codesmell

--- tests/RPC.Core.Tests/Validation/WriteRequestValidatorTests.cs
+++ tests/RPC.Core.Tests/Validation/WriteRequestValidatorTests.cs
@@ -34,7 +34,7 @@ public class WriteRequestValidatorTests
     [MemberData(nameof(TestData))]
     internal void Write_ShouldHaveValidationError_WhenInvalidParametersArePassed(string rpcUrl, int accountId, uint chainId, string to, HexBigInteger value, GasSettings gasSettings, string expectedErrorMessage)
     {
-        void testCode() => new RpcRequest(rpcUrl, accountId, chainId, to, value, gasSettings);
+        void testCode() { _ = new RpcRequest(rpcUrl, accountId, chainId, to, value, gasSettings); };
 
         var exception = Assert.Throws<ValidationException>(testCode);
         Assert.Equal(expectedErrorMessage, exception.Message);
