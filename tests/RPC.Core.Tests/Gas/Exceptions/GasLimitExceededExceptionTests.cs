6fe26642f1792baec59560a3050cc01f1a32f070
ArdenHide <ardenhide@gmail.com>
2023-06-05 17:55:40 +0300

    update GasLimitExceededExceptionTests

--- tests/RPC.Core.Tests/Gas/Exceptions/GasLimitExceededExceptionTests.cs
+++ tests/RPC.Core.Tests/Gas/Exceptions/GasLimitExceededExceptionTests.cs
@@ -1,19 +1,27 @@
 ﻿using Xunit;
-using System.Text.Json;
+using System.Runtime.Serialization;
 
 namespace RPC.Core.Gas.Exceptions.Tests;
 
 public class GasLimitExceededExceptionTests
 {
+    private class TestableGasLimitExceededException : GasLimitExceededException
+    {
+        public TestableGasLimitExceededException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+        public new void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+
     [Fact]
     public void GasLimitExceededException_SerializationTest()
     {
-        var originalException = new GasLimitExceededException();
-        GasLimitExceededException deserializedException;
+        var info = new SerializationInfo(typeof(GasLimitExceededException), new FormatterConverter());
+        var context = new StreamingContext();
 
-        var jsonString = JsonSerializer.Serialize(originalException);
-        deserializedException = JsonSerializer.Deserialize<GasLimitExceededException>(jsonString)!;
-
-        Assert.Equal(originalException.Message, deserializedException.Message);
+        var exception = new TestableGasLimitExceededException(info, context);
+        exception.GetObjectData(info, context);
     }
 }
