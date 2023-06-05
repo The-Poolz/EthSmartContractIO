2f3be40b99b775157b0299d6cff15c263a2eeab9
ArdenHide <ardenhide@gmail.com>
2023-06-05 17:19:18 +0300

    fix major codesmells

--- src/RPC.Core/Gas/Exceptions/GasLimitExceededException.cs
+++ src/RPC.Core/Gas/Exceptions/GasLimitExceededException.cs
@@ -1,6 +1,16 @@
-﻿namespace RPC.Core.Gas.Exceptions;
+﻿using System.Runtime.Serialization;
 
+namespace RPC.Core.Gas.Exceptions;
+
+[Serializable]
 public class GasLimitExceededException : Exception
 {
     public GasLimitExceededException() : base("Gas limit exceeded.") { }
+
+    protected GasLimitExceededException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+    }
 }
--- src/RPC.Core/Gas/Exceptions/GasPriceExceededException.cs
+++ src/RPC.Core/Gas/Exceptions/GasPriceExceededException.cs
@@ -1,6 +1,16 @@
-﻿namespace RPC.Core.Gas.Exceptions;
+﻿using System.Runtime.Serialization;
 
+namespace RPC.Core.Gas.Exceptions;
+
+[Serializable]
 public class GasPriceExceededException : Exception
 {
     public GasPriceExceededException() : base("Gas price exceeded.") { }
+
+    protected GasPriceExceededException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+    }
 }
