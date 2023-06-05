37cadce0193f7af54c69e7d6f4c745b549d8d7b6
ArdenHide <ardenhide@gmail.com>
2023-06-05 15:27:49 +0300

    add GasLimitChecker, update tests

---
+++ src/RPC.Core/Gas/GasLimitChecker.cs
@@ -0,0 +1,33 @@
+﻿using Nethereum.Util;
+using System.Numerics;
+using RPC.Core.Models;
+using Nethereum.RPC.Eth.DTOs;
+
+namespace RPC.Core.Gas;
+
+public static class GasLimitChecker
+{
+    public static void CheckGasLimits(TransactionInput transactionInput, GasSettings gasSettings)
+    {
+        CheckGasLimit(transactionInput, gasSettings);
+        CheckGasPrice(transactionInput, gasSettings);
+    }
+
+    private static void CheckGasLimit(TransactionInput transactionInput, GasSettings gasSettings)
+    {
+        if (transactionInput.Gas.Value > gasSettings.MaxGasLimit)
+        {
+            throw new InvalidOperationException("Gas limit exceeded.");
+        }
+    }
+
+    private static void CheckGasPrice(TransactionInput transactionInput, GasSettings gasSettings)
+    {
+        decimal etherValue = gasSettings.MaxGweiGasPrice * (decimal)Math.Pow(10, -9);
+        BigInteger weiValue = new UnitConversion().ToWei(etherValue);
+        if (transactionInput.GasPrice.Value > weiValue)
+        {
+            throw new InvalidOperationException("Gas price exceeded.");
+        }
+    }
+}
---
+++ tests/RPC.Core.Tests/Gas/GasLimitCheckerTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using RPC.Core.Models;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Hex.HexTypes;
+
+namespace RPC.Core.Gas.Tests;
+
+public class GasLimitCheckerTests
+{
+    [Fact]
+    internal void CheckGasLimits_LimitsNotExceeded_WithoutThrows()
+    {
+        var gasSettings = new GasSettings(30000, 6);
+        var transactionInput = new TransactionInput()
+        {
+            Gas = new HexBigInteger(27109),
+            GasPrice = new HexBigInteger(5000000000)
+        };
+
+        GasLimitChecker.CheckGasLimits(transactionInput, gasSettings);
+    }
+
+    [Fact]
+    internal void CheckGasLimits_GasLimitExceeded_ThrowException()
+    {
+        var gasSettings = new GasSettings(20000, 6);
+        var transactionInput = new TransactionInput()
+        {
+            Gas = new HexBigInteger(27109),
+            GasPrice = new HexBigInteger(5000000000)
+        };
+
+        void testCode() => GasLimitChecker.CheckGasLimits(transactionInput, gasSettings);
+
+        var exception = Assert.Throws<InvalidOperationException>(testCode);
+        Assert.Equal("Gas limit exceeded.", exception.Message);
+    }
+
+    [Fact]
+    internal void CheckGasLimits_GasPriceExceeded_ThrowException()
+    {
+        var gasSettings = new GasSettings(30000, 4);
+        var transactionInput = new TransactionInput()
+        {
+            Gas = new HexBigInteger(27109),
+            GasPrice = new HexBigInteger(5000000000)
+        };
+
+        void testCode() => GasLimitChecker.CheckGasLimits(transactionInput, gasSettings);
+
+        var exception = Assert.Throws<InvalidOperationException>(testCode);
+        Assert.Equal("Gas price exceeded.", exception.Message);
+    }
+}
