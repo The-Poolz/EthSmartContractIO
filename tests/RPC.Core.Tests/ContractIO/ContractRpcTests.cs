1f7bf48880094e17b955403162ecdd677777b273
ArdenHide <ardenhide@gmail.com>
2023-06-07 13:30:44 +0300

    update ContractRpcTests

--- tests/RPC.Core.Tests/ContractIO/ContractRpcTests.cs
+++ tests/RPC.Core.Tests/ContractIO/ContractRpcTests.cs
@@ -1,23 +1,40 @@
-﻿using Xunit;
+﻿using Moq;
+using Xunit;
+using RPC.Core.Gas;
 using RPC.Core.Models;
+using RPC.Core.Builders;
 using Flurl.Http.Testing;
 using Newtonsoft.Json.Linq;
 using RPC.Core.Tests.Mocks;
-using Org.BouncyCastle.Asn1.Ocsp;
+using RPC.Core.Transaction;
 using Nethereum.Hex.HexTypes;
-using Microsoft.Extensions.DependencyInjection;
-using Nethereum.Web3;
+using Nethereum.RPC.Eth.DTOs;
 
 namespace RPC.Core.ContractIO.Tests;
 
 public class ContractRpcTests
 {
     private const string RpcUrl = "http://localhost:8545/";
+    private readonly ContractRpc contractRpc = new();
+    private readonly RpcRequest readRequest = new (
+        rpcUrl: RpcUrl,
+        to: "0xA98b8386a806966c959C35c636b929FE7c5dD7dE",
+        data: "0xbef7a2f0"
+    );
+    private readonly RpcRequest writeRequest = new (
+        rpcUrl: "http://localhost:8545/",
+        to: "0xA98b8386a806966c959C35c636b929FE7c5dD7dE",
+        writeRequest: new WriteRpcRequest(
+            chainId: 1,
+            value: new HexBigInteger(10000000000000000),
+            gasSettings: new GasSettings(30000, 6),
+            accountProvider: MockMnemonicProvider.MnemonicProvider
+        )
+    );
 
     [Fact]
     internal void ExecuteAction_Read_ExpectedJsonString()
     {
-        var request = new RpcRequest(RpcUrl, "0xA98b8386a806966c959C35c636b929FE7c5dD7dE", "0xbef7a2f0");
         var response = new JObject()
         {
             { "jsonrpc", "2.0" },
@@ -29,31 +46,52 @@ public class ContractRpcTests
             .ForCallsTo(RpcUrl)
             .RespondWithJson(response);
 
-        var result = new ContractRpc().ExecuteAction(request);
+        var result = contractRpc.ExecuteAction(readRequest);
 
         Assert.NotNull(result);
         Assert.Equal(response["result"]?.ToString(), result);
     }
 
     [Fact]
-    internal void ExecuteAction_Write_ExpectedTransactionHex()
+    internal void ExecuteAction_WriteWithMockServices_ExpectedTransactionHex()
     {
-        var request = new RpcRequest(
-            rpcUrl: "http://localhost:8545/",
-            to: "0xA98b8386a806966c959C35c636b929FE7c5dD7dE",
-            writeRequest: new WriteRpcRequest(
-                chainId: 1,
-                value: new HexBigInteger(10000000000000000),
-                gasSettings: new GasSettings(30000, 6),
-                accountProvider: MockMnemonicProvider.MnemonicProvider
-            )
-        );
-        var contractRpc = new ContractRpc();
-        var serviceCollection = new ServiceCollection();
-        serviceCollection.AddSingleton<IWeb3>(MockWeb3.GetMock);
-        contractRpc.ServiceProvider = serviceCollection.BuildServiceProvider();
+        var mockGasEstimator = new Mock<IGasEstimator>();
+        mockGasEstimator.Setup(x => x.EstimateGas(It.IsAny<TransactionInput>()))
+            .Returns(new TransactionInput()
+            {
+                Gas = new HexBigInteger(29000),
+                GasPrice = new HexBigInteger(5000000000)
+            });
+        var mockGasPricer = new Mock<IGasPricer>();
+        mockGasPricer.Setup(x => x.GetCurrentWeiGasPrice())
+            .Returns(new HexBigInteger(5000000000));
+        var mockTransactionSigner = new Mock<ITransactionSigner>();
+        mockTransactionSigner.Setup(x => x.SignTransaction(It.IsAny<TransactionInput>()))
+            .Returns("signedTransaction");
+        var mockTransactionSender = new Mock<ITransactionSender>();
+        mockTransactionSender.Setup(x => x.SendTransaction("signedTransaction"))
+            .Returns("transactionHash");
+        contractRpc.ServiceProvider = new ServiceProviderBuilder()
+            .AddGasEstimator(mockGasEstimator.Object)
+            .AddGasPricer(mockGasPricer.Object)
+            .AddTransactionSigner(mockTransactionSigner.Object)
+            .AddTransactionSender(mockTransactionSender.Object)
+            .Build();
 
-        var result = contractRpc.ExecuteAction(request);
+        var result = contractRpc.ExecuteAction(writeRequest);
+
+        Assert.NotNull(result);
+        Assert.Equal("transactionHash", result);
+    }
+
+    [Fact]
+    internal void ExecuteAction_WriteWithMockWeb3_ExpectedTransactionHex()
+    {
+        contractRpc.ServiceProvider = new ServiceProviderBuilder()
+            .AddWeb3(MockWeb3.GetMock)
+            .Build();
+
+        var result = contractRpc.ExecuteAction(writeRequest);
 
         Assert.NotNull(result);
         Assert.Equal("transactionHash", result);
