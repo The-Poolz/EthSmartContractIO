fe45f6a3a2dc87ec9ae15ea376dc63aa213b66b4
ArdenHide <ardenhide@gmail.com>
2023-06-07 21:15:21 +0300

    test

--- src/RPC.Core/Builders/ServiceProviderBuilder.cs
+++ src/RPC.Core/Builders/ServiceProviderBuilder.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace RPC.Core.Builders;
 
-public class ServiceProviderBuilder
+public class ServiceProviderBuilder 
 {
     private readonly IServiceCollection services;
 
