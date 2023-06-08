6f1574c4e144f97d2cfadf50780db5b542320209
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-08 17:47:14 +0300

    add extensions

--- src/SmartContractIO.AccountProvider/PrivateKeyAccountProvider.cs
+++ src/SmartContractIO.AccountProvider/PrivateKeyAccountProvider.cs
@@ -6,9 +6,16 @@ namespace SmartContractIO.AccountProvider;
 
 public class PrivateKeyAccountProvider : IAccountProvider
 {
-    public Account Account { get; private set; }
+    public Account? Account { get; protected set; }
+
+    public PrivateKeyAccountProvider() { }
 
     public PrivateKeyAccountProvider(string privateKey, uint chainId)
+    {
+        SetAccount(privateKey, chainId);
+    }
+
+    public void SetAccount(string privateKey, uint chainId)
     {
         Account = new Account(privateKey, new HexBigInteger(chainId));
     }
---
+++ src/SmartContractIO.SecretsProvider/AccountProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SmartContractIO.AccountProvider;
+
+namespace SmartContractIO.SecretsProvider;
+
+public static class AccountProviderExtensions
+{
+    public static MnemonicAccountProvider UsingSecret(this MnemonicAccountProvider provider, ISecretsProvider secretsProvider, uint accountId, uint chainId, string seedPassword = "")
+    {
+        provider.SetAccount(secretsProvider.Secret, accountId, chainId, seedPassword);
+        return provider;
+    }
+
+    public static PrivateKeyAccountProvider UsingSecret(this PrivateKeyAccountProvider provider, ISecretsProvider secretsProvider, uint chainId)
+    {
+        provider.SetAccount(secretsProvider.Secret, chainId);
+        return provider;
+    }
+}
