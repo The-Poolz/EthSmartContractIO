6a41c2edc3143dd62ba0f22181f9d3061be1b953
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-08 16:28:56 +0300

    Add SmartContractIO.AccountProvider.Tests

---
+++ tests/SmartContractIO.AccountProvider.Tests/MnemonicAccountProviderTests.cs
@@ -0,0 +1,21 @@
+using Xunit;
+
+namespace SmartContractIO.AccountProvider.Tests;
+
+public class MnemonicAccountProviderTests
+{
+    [Fact]
+    public void Ctor_InitializesAccountWithGivenMnemonicWordsAccountIdAndChainId()
+    {
+        var mnemonicWords = "abandon amount liar amount expire adjust cage candy arch gather drum buyer";
+        var accountId = 0u;
+        var chainId = 1u;
+
+        var account = new MnemonicAccountProvider(mnemonicWords, accountId, chainId).Account;
+
+        Assert.NotNull(account);
+        Assert.NotNull(account.PrivateKey);
+        Assert.NotNull(account.ChainId);
+        Assert.Equal(chainId, account.ChainId.Value);
+    }
+}
