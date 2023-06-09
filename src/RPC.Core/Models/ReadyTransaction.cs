bb14051220d1830271bcfb7e4a5ac1aab556677c
Stan Goldin <salty87@gmail.com>
2023-06-09 13:01:05 +0300

    ReadyTransaction class

---
+++ src/RPC.Core/Models/ReadyTransaction.cs
@@ -0,0 +1,17 @@
+﻿using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using RPC.Core.Gas;
+
+namespace RPC.Core.Models;
+
+public class Transaction : TransactionInput
+{
+    public Transaction(RpcRequest request, GasPricer gasPricer) 
+        : base(request.Data, request.To, request.WriteRequest!.Value)
+    {
+        ChainId = new HexBigInteger(request.WriteRequest!.ChainId);
+        From = request.WriteRequest!.AccountProvider.Account.Address;
+        Gas = new HexBigInteger(request.WriteRequest!.GasSettings.MaxGasLimit);
+        GasPrice = gasPricer.GetCurrentWeiGasPrice();
+    }
+}
