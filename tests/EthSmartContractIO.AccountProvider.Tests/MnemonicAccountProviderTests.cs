7e974eea477453388c304afab398ef8d7b82bd76
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-12 18:15:27 +0300

    add tests

--- tests/EthSmartContractIO.AccountProvider.Tests/MnemonicAccountProviderTests.cs
+++ tests/EthSmartContractIO.AccountProvider.Tests/MnemonicAccountProviderTests.cs
@@ -1,17 +1,37 @@
+using Moq;
 using Xunit;
+using EthSmartContractIO.Providers;
 
 namespace EthSmartContractIO.AccountProvider.Tests;
 
 public class MnemonicAccountProviderTests
 {
+    private const string mnemonicWords = "abandon amount liar amount expire adjust cage candy arch gather drum buyer";
+
     [Fact]
     internal void Ctor_InitializesAccountWithGivenMnemonicWordsAccountIdAndChainId()
     {
-        var mnemonicWords = "abandon amount liar amount expire adjust cage candy arch gather drum buyer";
         var accountId = 0u;
         var chainId = 1u;
 
         var account = new MnemonicAccountProvider(mnemonicWords, accountId, chainId).Account;
+
+        Assert.NotNull(account);
+        Assert.NotNull(account.PrivateKey);
+        Assert.NotNull(account.ChainId);
+        Assert.Equal(chainId, account.ChainId.Value);
+    }
+
+    [Fact]
+    internal void Ctor_InitializesAccountWithGivenSecretsProviderAccountIdAndChainId()
+    {
+        var mockSecretsProvider = new Mock<ISecretsProvider>();
+        mockSecretsProvider.Setup(x => x.Secret)
+            .Returns(mnemonicWords);
+        var accountId = 0u;
+        var chainId = 1u;
+
+        var account = new MnemonicAccountProvider(mockSecretsProvider.Object, accountId, chainId).Account;
 
         Assert.NotNull(account);
         Assert.NotNull(account.PrivateKey);
--- tests/EthSmartContractIO.AccountProvider.Tests/PrivateKeyAccountProviderTests.cs
+++ tests/EthSmartContractIO.AccountProvider.Tests/PrivateKeyAccountProviderTests.cs
@@ -1,13 +1,32 @@
-﻿using Xunit;
+﻿using Moq;
+using Xunit;
+using EthSmartContractIO.Providers;
 
 namespace EthSmartContractIO.AccountProvider.Tests;
 
 public class PrivateKeyAccountProviderTests
 {
+    private const string privateKey = "0x1234";
+
     [Fact]
     internal void Ctor_InitializesAccountWithGivenPrivateKeyAndChainId()
     {
-        var privateKey = "0x1234";
+        var chainId = 1u;
+
+        var account = new PrivateKeyAccountProvider(privateKey, chainId).Account;
+
+        Assert.NotNull(account);
+        Assert.Equal(privateKey, account.PrivateKey);
+        Assert.NotNull(account.ChainId);
+        Assert.Equal(chainId, account.ChainId.Value);
+    }
+
+    [Fact]
+    internal void Ctor_InitializesAccountWithGivenSecretsProviderAndChainId()
+    {
+        var mockSecretsProvider = new Mock<ISecretsProvider>();
+        mockSecretsProvider.Setup(x => x.Secret)
+            .Returns(privateKey);
         var chainId = 1u;
 
         var account = new PrivateKeyAccountProvider(privateKey, chainId).Account;
