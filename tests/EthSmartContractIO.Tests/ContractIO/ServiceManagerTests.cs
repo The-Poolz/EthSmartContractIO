78bc1eccba990480b1f3d7ca1ddc54039b435f57
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-12 14:51:50 +0300

    add more coverage

--- tests/EthSmartContractIO.Tests/ContractIO/ServiceManagerTests.cs
+++ tests/EthSmartContractIO.Tests/ContractIO/ServiceManagerTests.cs
@@ -2,7 +2,7 @@
 using Nethereum.Hex.HexTypes;
 using EthSmartContractIO.Models;
 using EthSmartContractIO.Builders;
-using EthSmartContractIO.Tests.Mocks;
+using EthSmartContractIO.Providers.Account;
 
 namespace EthSmartContractIO.ContractIO.Tests;
 
@@ -36,12 +36,11 @@ public class ServiceManagerTests
             to: "0xA98b8386a806966c959C35c636b929FE7c5dD7dE",
             writeRequest: new WriteRpcRequest(
                 value: new HexBigInteger(10000000000000000),
-                gasSettings: new GasSettings(30000, 6),
-                accountParams: new object[] { "0x4e3c79ee2f53da4e456cb13887f4a7d59488677e9e48b6fb6701832df828f7e9", (uint)1 }
+                gasSettings: new GasSettings(30000, 6)
             )
         );
         var serviceProvider = new ServiceProviderBuilder()
-            .AddAccountProvider(new MockAccountProvider())
+            .AddAccountProvider(new MnemonicAccountProvider("over pull sibling only cage mean maid shrimp install travel reunion cabbage", (uint)1, (uint)1, "seed password"))
             .Build();
 
         var result = new ServiceManager(request, serviceProvider);
