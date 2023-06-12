3beb7b9fe41a790aa5502058d284a20fd4ada7af
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-12 17:50:49 +0300

    add more tests

--- tests/EthSmartContractIO.SecretsProvider.Tests/EnvironmentSecretProviderTests.cs
+++ tests/EthSmartContractIO.SecretsProvider.Tests/EnvironmentSecretProviderTests.cs
@@ -7,6 +7,26 @@ namespace EthSmartContractIO.SecretsProvider.Tests;
 public class EnvironmentSecretProviderTests
 {
     [Fact]
+    public void Constructor_WithDefaultParameters_CreatesInstance()
+    {
+        var mockSecretManager = new Mock<SecretManager>();
+
+        var secretProvider = new EnvironmentSecretProvider(mockSecretManager.Object);
+
+        Assert.NotNull(secretProvider);
+        Assert.Equal(mockSecretManager.Object, GetPrivateFieldValue<SecretManager>(secretProvider, "secretManager"));
+    }
+
+    [Fact]
+    public void Constructor_WithNullSecretManager_CreatesInstanceWithDefaultSecretManager()
+    {
+        var secretProvider = new EnvironmentSecretProvider();
+
+        Assert.NotNull(secretProvider);
+        Assert.IsType<SecretManager>(GetPrivateFieldValue<SecretManager>(secretProvider, "secretManager"));
+    }
+
+    [Fact]
     internal void Secret_WhenCalled_UsesSecretManagerGetSecretValue()
     {
         Environment.SetEnvironmentVariable("SECRET_ID", "secretId");
@@ -20,5 +40,11 @@ public class EnvironmentSecretProviderTests
 
         Assert.Equal("secretValue", secret);
         mockSecretManager.Verify(x => x.GetSecretValue("secretId", "secretKey"), Times.Once());
+    }
+
+    private static T GetPrivateFieldValue<T>(object obj, string fieldName)
+    {
+        var fieldInfo = obj.GetType().GetField(fieldName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        return (T)fieldInfo.GetValue(obj);
     }
 }
