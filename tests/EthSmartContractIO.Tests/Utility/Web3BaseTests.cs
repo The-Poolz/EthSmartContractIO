86dd6201c1e1e5c60d19a6785654e278e5fd840f
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-13 10:20:55 +0300

    cleanup Web3BaseTests

--- tests/EthSmartContractIO.Tests/Utility/Web3BaseTests.cs
+++ tests/EthSmartContractIO.Tests/Utility/Web3BaseTests.cs
@@ -9,8 +9,9 @@ public class Web3BaseTests
     [Fact]
     internal void CreateWeb3()
     {
+        string privateKey = "0x4e3c79ee2f53da4e456cb13887f4a7d59488677e9e48b6fb6701832df828f7e9";
         string rpcConnection = "http://localhost:8545/";
-        Account account = new("0xprivateKey");
+        Account account = new(privateKey);
 
         var web3 = Web3Base.CreateWeb3(rpcConnection, account);
 
