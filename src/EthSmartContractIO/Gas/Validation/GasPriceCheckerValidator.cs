a1c07ae57ee7eb706e10d42a9ac512132fae43f2
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-06-13 19:03:23 +0300

    add xml for validators

--- src/EthSmartContractIO/Gas/Validation/GasPriceCheckerValidator.cs
+++ src/EthSmartContractIO/Gas/Validation/GasPriceCheckerValidator.cs
@@ -2,8 +2,14 @@
 
 namespace EthSmartContractIO.Gas.Validation;
 
+/// <summary>
+/// Validator for gas price checks. It validates that the <see cref="GasPriceChecker.GasPrice"/> is not greater than the <see cref="GasPriceChecker.MaxGweiGasPrice"/>.
+/// </summary>
 public class GasPriceCheckerValidator : AbstractValidator<GasPriceChecker>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GasPriceCheckerValidator"/> class.
+    /// </summary>
     public GasPriceCheckerValidator()
     {
         RuleFor(x => x.MaxGweiGasPrice)
--- src/EthSmartContractIO/Models/Validation/BaseRequestValidator.cs
+++ src/EthSmartContractIO/Models/Validation/BaseRequestValidator.cs
@@ -4,12 +4,20 @@ using System.Text.RegularExpressions;
 
 namespace EthSmartContractIO.Models.Validation;
 
+/// <summary>
+/// Base class for validating RPC requests.<br/>
+/// It validates the <see cref="RpcRequest.To"/> property to ensure it's a valid Ethereum address,
+/// and the <see cref="RpcRequest.RpcUrl"/> property to ensure it's a valid URL.
+/// </summary>
 public class BaseRequestValidator : AbstractValidator<RpcRequest>
 {
     protected const string MethodSignaturePattern = @"^0x[0-9a-fA-F]{8}";
     protected const string ParametersPattern = @"([0-9a-fA-F]{64})*$";
     protected static readonly Regex EthereumDataPattern = new($"{MethodSignaturePattern}{ParametersPattern}", default, TimeSpan.FromMinutes(1));
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseRequestValidator"/> class.
+    /// </summary>
     public BaseRequestValidator()
     {
         RuleFor(x => x.To)
