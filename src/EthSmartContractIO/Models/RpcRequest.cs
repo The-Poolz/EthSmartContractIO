ab13af34759a9c2fabc6e50ccfd1c6306265f8c3
Stanislav Vysotskyi <ardenhide@gmail.com>
2023-07-31 11:29:38 +0300

    Revert "update ctor"
    This reverts commit 53814d56166702c2f78a610d8ecb8fca5fc3709f.

--- src/EthSmartContractIO/Models/RpcRequest.cs
+++ src/EthSmartContractIO/Models/RpcRequest.cs
@@ -9,26 +9,45 @@ namespace EthSmartContractIO.Models;
 public class RpcRequest
 {
     public bool ActionIsRead => WriteRequest == null;
-    public string RpcUrl { get; }
-    public string To { get; }
-    public string Data { get; }
-    public WriteRpcRequest? WriteRequest { get; }
+    public string RpcUrl { get; private set; }
+    public string To { get; private set; }
+    public string Data { get; private set; }
+    public WriteRpcRequest? WriteRequest { get; private set; }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="RpcRequest"/> class for a read operation or for a write operation if <see cref="WriteRpcRequest"/> not null.
+    /// Initializes a new instance of the <see cref="RpcRequest"/> class for a read operation.
     /// </summary>
     /// <param name="rpcUrl">The URL of the RPC server.</param>
     /// <param name="to">The address to send the request to.</param>
     /// <param name="data">The data to send with the request.</param>
-    /// <param name="writeRequest">The write request to execute.</param>
-    public RpcRequest(string rpcUrl, string to, string data = "", WriteRpcRequest? writeRequest = null)
+    public RpcRequest(string rpcUrl, string to, string data)
     {
         RpcUrl = rpcUrl;
         To = to;
         Data = data;
+
+        new ReadRequestValidator().ValidateAndThrow(this);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RpcRequest"/> class for a write operation.
+    /// </summary>
+    /// <param name="rpcUrl">The URL of the RPC server.</param>
+    /// <param name="to">The address to send the request to.</param>
+    /// <param name="writeRequest">The write request to execute.</param>
+    /// <param name="data">The data to send with the request.</param>
+    public RpcRequest(
+        string rpcUrl,
+        string to,
+        WriteRpcRequest writeRequest,
+        string? data = null
+    )
+    {
+        RpcUrl = rpcUrl;
+        To = to;
+        Data = data ?? string.Empty;
         WriteRequest = writeRequest;
 
-        BaseRequestValidator validator = ActionIsRead ? new ReadRequestValidator() : new WriteRequestValidator();
-        validator.ValidateAndThrow(this);
+        new WriteRequestValidator().ValidateAndThrow(this);
     }
 }
