2fac21ff4c946a3235a2af20a49d74afeb7db0c6
Stan Goldin <salty87@gmail.com>
2023-08-13 16:12:23 +0300

    make it arrow

--- src/EthSmartContractIO/ContractIO/ContractReader.cs
+++ src/EthSmartContractIO/ContractIO/ContractReader.cs
@@ -15,10 +15,7 @@ public class ContractReader : IContractIO
     /// Initializes a new instance of the <see cref="ContractReader"/> class.
     /// </summary>
     /// <param name="request">The <see cref="RpcRequest"/> to execute.</param>
-    public ContractReader(RpcRequest request)
-    {
-        this.request = request;
-    }
+    public ContractReader(RpcRequest request) => this.request = request;
 
     /// <summary>
     /// Executes a read action on the Ethereum network.
@@ -26,36 +23,25 @@ public class ContractReader : IContractIO
     /// <returns>The result of the action.</returns>
     /// <exception cref="FlurlHttpException">Thrown when the HTTP request fails.</exception>
     /// <exception cref="KeyNotFoundException">Thrown when the response does not contain the key 'result'.</exception>
-    public virtual string RunContractAction()
-    {
-        var input = CreateActionInput();
-
-        var response = request.RpcUrl.PostJsonAsync(input)
-            .GetAwaiter()
-            .GetResult();
-
-        return ParseResponse(response);
-    }
-
+    public virtual string RunContractAction() => ExtractResultFromResponse(Task.Run(() => PostRequestAsync()).Result);
     /// <summary>
     /// Creates the input for the read action.
     /// </summary>
     /// <returns>The created <see cref="ReadRpcRequest"/>.</returns>
-    private ReadRpcRequest CreateActionInput() =>
+    private ReadRpcRequest CreateActionInput =>
         new(request.To, request.Data);
-
+    /// <summary>
+    /// Sends the HTTP request to the Ethereum network.
+    /// </summary>
+    /// <returns></returns>
+    private async Task<JObject> PostRequestAsync() =>
+         await request.RpcUrl.PostJsonAsync(CreateActionInput).ReceiveJson<JObject>();
     /// <summary>
     /// Parses the response from the Ethereum network.
     /// </summary>
-    /// <param name="flurlResponse">The response to parse.</param>
+    /// <param name="response">The response to parse.</param>
     /// <returns>The parsed response.</returns>
     /// <exception cref="KeyNotFoundException">Thrown when the response does not contain the key 'result'.</exception>
-    private static string ParseResponse(IFlurlResponse flurlResponse)
-    {
-        var response = flurlResponse.GetJsonAsync<JObject>()
-            .GetAwaiter()
-            .GetResult();
-
-        return response["result"]?.ToString() ?? throw new KeyNotFoundException("Response does not contain the key 'result'.");
-    }
+    private static string ExtractResultFromResponse(JObject response) =>
+        response["result"]?.ToString() ?? throw new KeyNotFoundException("Response does not contain the key 'result'.");
 }
